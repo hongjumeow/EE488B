d7a4e927e4d8e382b4b808808e3e87d2d64e87a0
hongjumeow <hongjuj0329@kaist.ac.kr>
2022-11-20 12:24:20 +0900

    fix embedding size

--- EmbedNet.py
+++ EmbedNet.py
@@ -18,7 +18,7 @@ class EmbedNet(nn.Module):
         
         if model == "MobileFaceNet":
             EmbedNetModel = MobileFaceNet
-            self.__S__ = EmbedNetModel(embedding_size=kwargs.nOut);
+            self.__S__ = EmbedNetModel(embedding_size=512);
 
         else:
             ## __S__ is the embedding model
